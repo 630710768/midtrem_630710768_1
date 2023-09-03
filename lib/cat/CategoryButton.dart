c737fde5235817f7724bf84807082d86f15e8971
630710768 <toswittawas@gmail.com>
2023-09-03 13:48:02 +0700

    commit

--- lib/cat/CategoryButton.dart
+++ lib/cat/CategoryButton.dart
@@ -1,42 +1,41 @@
+// lib/widgets/category_button.dart
 import 'package:flutter/material.dart';
 
 class CategoryButton extends StatelessWidget {
   final String text;
-
   final IconData icon;
   final Color color;
-
- // final String imagePath;
+  final bool isSelected;
 
   const CategoryButton({
-    super.key,
+    Key? key,
     required this.text,
     required this.icon,
     required this.color,
-
-    //required this.imagePath,
+  required this.isSelected,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      width: 100,
+      height: 120,
+      width: 120,
       decoration: BoxDecoration(
-           color : Colors.white,
-          borderRadius: BorderRadius.circular(12.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.3),
-              spreadRadius: 7,
-              blurRadius: 12,
-              offset: Offset(0, 10), // changes position of shadow
-            ),
-          ],
-          border: Border.all(
-            color: Colors.black,
-            width: 3.0,
-          )),
+        color: isSelected ? color : Colors.white,
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 7,
+            blurRadius: 12,
+            offset: Offset(0, 10),
+          ),
+        ],
+        border: Border.all(
+          color: isSelected ? Colors.black : Colors.grey,
+          width: 3.0,
+        ),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -44,12 +43,12 @@ class CategoryButton extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             margin: const EdgeInsets.all(12.0),
             decoration: BoxDecoration(
+              color: isSelected ? color : Colors.white,
               shape: BoxShape.circle,
             ),
             child: Icon(
               icon,
-             color : Colors.black,
-
+              color: Colors.black,
             ),
           ),
           Text(
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -2,8 +2,10 @@
 
 import 'package:flutter/material.dart';
 
+import '../../cat/CategoryButton.dart';
+
 // TODO: ใส่รหัสนักศึกษาที่ค่าสตริงนี้
-const studentId = 'Student ID';
+const studentId = '630710768';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,9 +15,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+
   @override
   Widget build(BuildContext context) {
-    var textTheme = Theme.of(context).textTheme;
+    var textTheme = Theme
+        .of(context)
+        .textTheme;
 
     return Scaffold(
       body: SafeArea(
@@ -32,33 +38,117 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text('Good Morning',
-                  textAlign: TextAlign.center, style: textTheme.headlineMedium),
-              Text(studentId,
-                  textAlign: TextAlign.center,
-                  style: textTheme.headlineSmall!.copyWith(
-                      fontWeight: FontWeight.bold, color: Colors.black87)),
-              Spacer(),
-              _buildQuizView(),
-              Spacer(),
-              _buildButtonPanel(),
-              SizedBox(height: 16.0),
-            ],
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text('Good Morning',
+                    textAlign: TextAlign.center,
+                    style: textTheme.headlineMedium),
+                Text(studentId,
+                    textAlign: TextAlign.center,
+                    style: textTheme.headlineSmall!.copyWith(
+                        fontWeight: FontWeight.bold, color: Colors.black87)),
+                Spacer(),
+                _buildQuizView(),
+                Spacer(),
+                _buildButtonPanel(),
+                SizedBox(height: 16.0),
+              ]
           ),
+
+
         ),
       ),
     );
   }
 
   _buildQuizView() {
-    // TODO: build UI
-    return Center(child: Text('TODO: build UI'));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          padding: EdgeInsets.all(10.0),
+          margin: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Colors.lightBlue,
+            borderRadius: BorderRadius.circular(12.0),
+            border: Border.all(
+              color: Colors.black,
+              width: 2.0,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              '2+2 = ?',
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ),
+        SizedBox(height: 10.0),
+        CategoryButton(
+          text: 'A.4',
+          color: Colors.lime,
+          icon: Icons.cabin,
+          isSelected: true,
+        ),
+        SizedBox(height: 10.0),
+        CategoryButton(
+          text: 'B.3',
+          color: Colors.lime,
+          icon: Icons.cabin,
+          isSelected: false,
+        ),
+        SizedBox(height: 10.0),
+        CategoryButton(
+          text: 'C.2',
+          icon: Icons.star,
+          color: Colors.orange,
+          isSelected: false,
+        ),
+        SizedBox(height: 10.0),
+        CategoryButton(
+          text: 'D.1',
+          icon: Icons.bookmark,
+          color: Colors.green,
+          isSelected: false,
+        ),
+      ],
+    );
   }
 
+
   _buildButtonPanel() {
-    // TODO: build UI
-    return Center(child: Text('TODO: build UI'));
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Colors.amberAccent, //
+              minimumSize: Size(120.0, 48.0),
+            ),
+            child: Text('Back',style: TextStyle(color: Colors.black),),
+          ),
+          SizedBox(width: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+
+              });
+            },
+    style: ElevatedButton.styleFrom(
+    primary: Colors.amberAccent, //
+    minimumSize: Size(120.0, 48.0),
+      ),
+    child: Text('Next',style: TextStyle(color: Colors.black),),
+          ),
+        ],
+      ),
+    );
   }
+
 }
